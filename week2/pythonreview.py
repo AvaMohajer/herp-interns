25c0219d7bca895570ff14dd8d27b4cc7f8806df
Ava Mohajer <amohajer@BridgeUp-028.local>
2016-11-01 18:39:51 -0400

    added python review

---
+++ week2/pythonreview.py
@@ -0,0 +1,18 @@
+#def square(x):
+#	x*x
+#	print x*x
+#	return
+#square(7)	
+
+#def favoritecolor(name):
+#	dictionary = {'Ava':'Blue', 'Crystal':'Blue', 'Elena':'Silver', 'Odelia':'NA', 'Renee':'Purple', 'Stephanie':'Blue'} 
+#	print dictionary[name]
+#	return
+#favoritecolor('Ava')	
+
+x = [1, 2, 3, 4]
+def reverse(x):
+	#x.reverse()
+	for i in reversed(x):
+	print x,
+reverse(x)
